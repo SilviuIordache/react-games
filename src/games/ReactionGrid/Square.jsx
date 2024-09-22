f01ca0078e2ed1e6a6253b0e7fc229fc94f93e5f
SilviuIordache <silviu.iordache92@gmail.com>
2024-09-22 16:28:05 +0300

    Add grid and random coordinates

---
+++ src/games/ReactionGrid/Square.jsx
@@ -0,0 +1,5 @@
+export const Square = () => {
+  return (
+    <div className="bg-gray border border-gray-500 border-b-1 w-6 h-6 hover:bg-gray-600"></div>
+  );
+};
--- src/games/ReactionGrid/index.jsx
+++ src/games/ReactionGrid/index.jsx
@@ -1,3 +1,26 @@
+import { Square } from './Square';
+
 export default function ReactionGrid() {
-  return <div>ReactionGrid</div>;
+  const gridSize = 20;
+
+  const arr = Array.from({ length: gridSize });
+
+  const getRandomCoordinate = (max) => {
+    return Math.floor(Math.random() * max);
+  };
+
+  const x = getRandomCoordinate(gridSize);
+  const y = getRandomCoordinate(gridSize);
+
+  console.log(x);
+
+  const row = arr.map((item, index) => {
+    return <Square key={index} />;
+  });
+
+  const grid = arr.map((item, index) => {
+    return <div key={index}>{row}</div>;
+  });
+
+  return <div className="flex">{grid}</div>;
 }
