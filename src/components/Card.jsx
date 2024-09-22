87ae1c0f31e4e879d04f5783c0d35c12f5320339
SilviuIordache <silviu.iordache92@gmail.com>
2024-09-22 14:48:15 +0300

    Make cards nicer

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@
 
 export const Card = ({ children, onClick }) => {
   return (
-    <div className="bg-gray-700 shadow-md rounded-lg p-6 w-32 h-32 text-center" onClick={onClick}>
+    <div className="bg-gray-700 shadow-md rounded-lg p-6 w-32 h-32 text-center cursor-pointer hover:bg-gray-600" onClick={onClick}>
       {children}
     </div>
   );
