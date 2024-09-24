daee4e30d012fb2d483d5e1c67ea01aceedf082f
SilviuIordache <silviu.iordache92@gmail.com>
2024-09-24 15:17:32 +0300

    Refactor using useReducer

---
+++ src/games/ReactionGrid/gameReducer.ts
@@ -0,0 +1,61 @@
+export enum GameState {
+  START = 'START',
+  PLAYING = 'PLAYING',
+  END = 'END',
+}
+
+export type GameStateType = keyof typeof GameState;
+
+export type ReducerAction =
+  | { type: 'SET_GRID_SIZE'; payload: number }
+  | { type: 'START_GAME' }
+  | { type: 'CHANGE_COORDS' }
+  | { type: 'ADD_REACTION_TIME'; payload: number }
+  | { type: 'DECREMENT_CLICKS' }
+  | { type: 'SET_END_GAME' }
+  | { type: 'RESET_GAME' };
+
+export const initialState = {
+  clicksToMeasure: 5,
+  reactionArr: [],
+  gameState: GameState.START,
+  gridSize: 20,
+  gridSizeMobile: 10,
+  coords: { x: -1, y: -1 },
+  timer: null,
+};
+
+export const gameReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_GRID_SIZE':
+      return { ...state, gridSize: action.payload };
+    case 'START_GAME':
+      return {
+        ...state,
+        clicksToMeasure: initialState.clicksToMeasure,
+        reactionArr: [],
+        gameState: GameState.PLAYING,
+      };
+    case 'CHANGE_COORDS':
+      const getRandomCoordinate = (max) => Math.floor(Math.random() * max);
+      const newCoords = {
+        x: getRandomCoordinate(state.gridSize),
+        y: getRandomCoordinate(state.gridSize),
+      };
+
+      return { ...state, coords: newCoords, timer: Date.now() };
+    case 'ADD_REACTION_TIME':
+      return {
+        ...state,
+        reactionArr: [...state.reactionArr, action.payload],
+      };
+    case 'DECREMENT_CLICKS':
+      return { ...state, clicksToMeasure: state.clicksToMeasure - 1 };
+    case 'SET_END_GAME':
+      return { ...state, gameState: GameState.END };
+    case 'RESET_GAME':
+      return initialState;
+    default:
+      return state;
+  }
+};
--- src/games/ReactionGrid/index.tsx
+++ src/games/ReactionGrid/index.tsx
@@ -1,75 +1,54 @@
-import React, { useCallback, useEffect, useMemo, useReducer, useRef, useState } from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useReducer,
+  useRef,
+} from 'react';
 import useDeviceSize from '../../custom-hooks/useDeviceSize';
 import { EndDialog } from './EndDialog';
-import { GameState, gameStateReducer } from './gameStateReducer';
+import { GameState, gameReducer, initialState } from './gameReducer';
 import { convertMsToSeconds } from './helpers';
 import { Square } from './Square';
 import { StartDialog } from './StartDialog';
 
 export default function ReactionGrid() {
-  // TODO: 30x30 for large screens, 10x10 for mobile
-  const initialGridSize = 20;
-  const clicksCount = 20;
-
-  const [gridSize, setGridSize] = useState(initialGridSize);
-
   const isSmallDevice = useDeviceSize();
-
-  useEffect(() => {
-    if (isSmallDevice) {
-      setGridSize(10);
-    } else {
-      setGridSize(20);
-    }
-  }, [isSmallDevice]);
-
-  useEffect(() => {
-    console.log(isSmallDevice);
-  }, [isSmallDevice]);
-
   const timerRef = useRef<number | null>(null);
 
-  const [reactionArr, setReactionArr] = useState<number[]>([]);
-
-  const [clicksToMeasure, setClicksToMeasure] = useState(clicksCount);
-  const [coords, setCoords] = useState({ x: -1, y: -1 });
-
-  const [gameState, setGameState] = useReducer(
-    gameStateReducer,
-    GameState.START
-  );
+  const [state, dispatch] = useReducer(gameReducer, initialState);
 
   useEffect(() => {
-    if (reactionArr.length === clicksCount) {
-      setGameState('setEnd');
+    dispatch({
+      type: 'SET_GRID_SIZE',
+      payload: isSmallDevice ? state.gridSizeMobile : state.gridSize,
+    });
+  }, [isSmallDevice]);
+
+  useEffect(() => {
+    if (state.clicksToMeasure === 0) {
+      dispatch({ type: 'SET_END_GAME' });
     }
-  }, [reactionArr]);
+  }, [state.clicksToMeasure]);
 
   const averageReactionTime = useMemo(() => {
-    const sum = reactionArr.reduce((sum, value) => sum + value, 0);
+    const sum = state.reactionArr.reduce((sum, value) => sum + value, 0);
 
-    const avg = sum / reactionArr.length;
+    const avg = sum / state.reactionArr.length;
 
     return avg;
-  }, [reactionArr]);
+  }, [state.reactionArr]);
 
-  const getRandomCoordinate = (max) => {
-    return Math.floor(Math.random() * max);
-  };
-
-  const changeCoords = useCallback(() => {
-    const newX = getRandomCoordinate(gridSize);
-    const newY = getRandomCoordinate(gridSize);
-
-    setCoords({ x: newX, y: newY });
+  const changeCoords = () => {
+    dispatch({ type: 'CHANGE_COORDS' });
 
     timerRef.current = Date.now();
-  }, [gridSize]);
+  };
 
   const updateReactionTimes = (isHighlightedSquare) => {
-    if (gameState === GameState.PLAYING) {
+    if (state.gameState === GameState.PLAYING) {
       if (isHighlightedSquare) {
-        setClicksToMeasure((clicksToMeasure) => clicksToMeasure - 1);
+        dispatch({ type: 'DECREMENT_CLICKS' });
 
         const reactionTime = Date.now();
 
@@ -77,25 +56,26 @@ export default function ReactionGrid() {
           ? reactionTime - timerRef.current
           : reactionTime;
 
-        setReactionArr((prevArr) => [...prevArr, reactionDuration]);
+        dispatch({ type: 'ADD_REACTION_TIME', payload: reactionDuration });
         changeCoords();
       } else {
-        if (reactionArr.length > 0) {
+        if (state.reactionArr.length > 0) {
           const penaltyReaction =
             averageReactionTime + (averageReactionTime * 20) / 100;
 
-          setReactionArr((prevArr) => [...prevArr, penaltyReaction]);
+          dispatch({ type: 'ADD_REACTION_TIME', payload: penaltyReaction });
         }
       }
     }
   };
 
-  const grid = useMemo(() => {
+  const Grid = () => {
     const tempGrid: JSX.Element[] = [];
-    for (let i = 0; i < gridSize; i++) {
+    for (let i = 0; i < state.gridSize; i++) {
       const row: JSX.Element[] = [];
-      for (let j = 0; j < gridSize; j++) {
-        const isHighlightedSquare = i === coords.x && j === coords.y;
+      for (let j = 0; j < state.gridSize; j++) {
+        const isHighlightedSquare =
+          i === state.coords.x && j === state.coords.y;
         row.push(
           <Square
             key={`${i}-${j}`}
@@ -107,12 +87,10 @@ export default function ReactionGrid() {
       tempGrid.push(<div key={i}>{row}</div>);
     }
     return tempGrid;
-  }, [changeCoords, coords.x, coords.y]);
+  };
 
   const handleStartGame = () => {
-    setClicksToMeasure(clicksCount);
-    setReactionArr([]);
-    setGameState('setPlaying');
+    dispatch({ type: 'START_GAME' });
     changeCoords();
   };
 
@@ -120,12 +98,12 @@ export default function ReactionGrid() {
     <div>
       <StartDialog
         onStartGame={handleStartGame}
-        isOpen={gameState === GameState.START}
+        isOpen={state.gameState === GameState.START}
       />
 
       <EndDialog
-        reactionArr={reactionArr}
-        isOpen={gameState === GameState.END}
+        reactionArr={state.reactionArr}
+        isOpen={state.gameState === GameState.END}
         onRestartGame={handleStartGame}
         averageReactionTime={averageReactionTime}
       />
@@ -141,10 +119,12 @@ export default function ReactionGrid() {
 
       <div className="flex justify-between mb-1">
         <div>Avg: {convertMsToSeconds(averageReactionTime) || '-'}</div>
-        <div>Clicks left: {clicksToMeasure}</div>
+        <div>Clicks left: {state.clicksToMeasure}</div>
       </div>
 
-      <div className="flex">{grid}</div>
+      <div className="flex">
+        <Grid />
+      </div>
     </div>
   );
 }
