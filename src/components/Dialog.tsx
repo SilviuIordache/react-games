2e6c5b473d27798237cfab9a395284be759432e0
SilviuIordache <silviu.iordache92@gmail.com>
2025-02-08 21:40:09 +0200

    Add icon package + close icon to the Dialog

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -44,7 +44,27 @@ const Dialog: React.FC<Props> = ({
       <div className="bg-gray-800 p-4 rounded-lg shadow-lg max-w-sm w-full text-start">
         <div className="flex justify-between items-start">
           <h2 className="text-xl font-semibold mb-4">{title}</h2>
-          {showCloseTopButton && <button onClick={handleClose}>x</button>}
+          {showCloseTopButton && (
+            <button
+              onClick={handleClose}
+              className="transition duration-200 ease-in-out transform hover:scale-105 active:scale-95"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-6 transition duration-200 ease-in-out hover:stroke-2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          )}
         </div>
         {children}
         <div className="flex justify-end">
