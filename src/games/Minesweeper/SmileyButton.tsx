0761ccae883ac16335cbc04cb6ada1ab339dc0c9
SilviuIordache <silviu.iordache92@gmail.com>
2025-02-24 10:19:35 +0200

    Fix

--- src/games/Minesweeper/SmileyButton.tsx
+++ src/games/Minesweeper/SmileyButton.tsx
@@ -34,7 +34,7 @@ const SmileyButton = ({
       {/* <div>GameState: {gameState}</div> */}
 
       <button
-        className="text-2xl p-2 px-3 bg-gray-700 border border-gray-500  hover:bg-gray-400 active:bg-gray-900 rounded-md "
+        className="text-2xl p-2 px-3 bg-gray-700 border border-gray-500 hover:bg-gray-400 active:bg-gray-900 rounded-md"
         onClick={handleStartGame}
       >
         {emoji()}
